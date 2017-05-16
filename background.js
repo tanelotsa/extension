88eecf70fc7b9bacfdb748d077e9a1ec025ef1f2
tanelotsa <tanelotsa@users.noreply.github.com>
2017-05-16 23:37:45 +0300

    esimese õhtu lamine timmimime
    123

---
+++ background.js
@@ -0,0 +1,6 @@
+console.log("extension loaded");
+var keyWords = [];
+for (var i = 0; i < document.getElementsByTagName('a').length; i++) {
+  document.getElementsByTagName('a')[i].style.border = "3px solid red";
+
+}
